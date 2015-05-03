0518c1d8051039886f070e5290b280134a375855
Brian Schmitz <bs137308@gmail.com>
2015-05-02 21:20:51 -0700

    Orders  view

---
+++ application/views/admin/orders.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+	<title>Orders</title>
+	<title>Home</title>
+	<meta charset="utf-8" />
+	<meta name="description" content="This website is using Twitter Bootstrap"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="/assets/bootstrap/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="/assets/bootstrap/js/bootstrap.min.js">
+	<link rel="stylesheet" type="text/css" href="/assets/welcome.css"> 
+	<!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet"> -->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+ </head>
+ <body>	
+ 	<?php $this->load->view('partials/header'); ?>
+ 	<div class="container">
+
+	</div>
+</body>
+
+
+
+
+
+
+
+</html>
