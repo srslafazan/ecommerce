2b6f7123bc3fc9ed42ce521f75b8555680868e3a
Brian Schmitz <bs137308@gmail.com>
2015-05-04 18:18:55 -0700

    Product page functionality added

--- application/config/routes.php
+++ application/config/routes.php
@@ -6,7 +6,7 @@ $route['404_override'] = '';
 // $route['(:any)/(:any)'] = 'products/get_product/$1/$2';
 // end of main routes
 $route['admin'] = 'admin';
-$route['products/show/(:any)'] = 'products/product_view/$1';
+$route['products/show/(:any)'] = 'products/show/$1';
 $route['products/browse'] = 'products/get_product';
 $route['products/search_by_price'] = 'products/product_search';
 
--- application/views/customers/shows.php
+++ application/views/customers/shows.php
@@ -10,49 +10,35 @@
     <link rel="stylesheet" type="text/css" href="/assets/bootstrap/js/bootstrap.min.js">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="/assets/product_page.css">
+    <script>
+        $(document).ready(function (){
+            $(document).on('click', '#big_pic' function(){
+                $(this).attr('src', "/assets/images/<?= $product['photo_url']  ?>");
+             });
+        });  
+    </script>
 </head>
 <body>
 <!-- nav -->
-<?php $this->load->view('partials/header'); ?>
+<?php $this->load->view('partials/header');?>
+
     <div class='container'>
         <a class="header" href='/'>Go Back</a>
         <div class="row">
             <div class="col-sm-6">
-                <h2 class="header">Black Belt for Staff</h2>
-                <img id="big_pic" src='#' alt='product image'>
+                
+                <h2 class="header"><?= $products[0]['name'] ?></h2>
+                <a href="#"><img id="big_pic" src="/assets/images/<?=$products[0]['photo_url']?>" alt='product image'></a>
                 <div class='thumbnails'>
-                    <img id="first" class="prod_thumbnails" src='#' alt='product thumbnail'>
-                    <img class="prod_thumbnails" src='#' alt='product thumbnail'>
-                    <img class="prod_thumbnails" src='#' alt='product thumbnail'>
-                    <img class="prod_thumbnails" src='#' alt='product thumbnail'>
-                    <img class="prod_thumbnails" src='#' alt='product thumbnail'>
+                    <?php  foreach ($products as $product) {?>
+                    <a href="#"><img class="prod_thumbnails" src="/assets/images/<?= $product['photo_url']  ?>" alt='product thumbnail'></a>
+                    <?php } ?>
                 </div>
             </div>
     
 
         <div class="col-md-6 description">
-                <p class>
-                Description...Description...Description...Description...Description...
-                Description...Description...Description...Description...Description...
-                Description...Description...Description...Description...Description...
-                Description...Description...Description...Description...Description...
-                Description...Description...Description...Description...Description...
-                Description...Description...Description...Description...Description...
-                Description...Description...Description...Description...Description...
-                Description...Description...Description...Description...Description...
-                Description...Description...Description...Description...Description...
-                Description...Description...Description...Description...Description...
-                Description...Description...Description...Description...Description...
-                Description...Description...Description...Description...Description...
-                Description...Description...Description...Description...Description...
-                Description...Description...Description...Description...Description...
-                Description...Description...Description...Description...Description...
-                Description...Description...Description...Description...Description...
-                Description...Description...Description...Description...Description...
-                Description...Description...Description...Description...Description...
-                Description...Description...Description...Description...Description...
-                Description...Description...Description...Description...Description...
-                </p>
+                <p><?=  $products[0]['description']  ?></p>
         </div>
     </div>
 
@@ -63,11 +49,11 @@
                     <label for="quantity" class="col-sm-2 control-label">Buy</label>
                     <div class="col-sm-7">
                          <select class="form-control">
-                              <option>1</option>
-                              <option>2</option>
-                              <option>3</option>
-                              <option>4</option>
-                              <option>5</option>
+                              <option>1 for $<?= $products[0]['price'] ?></option>
+                              <option>2 for $<?= ($products[0]['price'] * 2)?></option>
+                              <option>3 for $<?= ($products[0]['price'] * 3) ?></option>
+                              <option>4 for $<?= ($products[0]['price'] * 4) ?></option>
+                              <option>5 for $<?= ($products[0]['price'] * 5) ?></option>
                         </select>
                     </div>
                 </div>
@@ -77,42 +63,19 @@
     
 
         
-    <div class="row">
-            <form class='products'>
-                <a href='#'><img src='#' alt='products'></a>
-                <div>price overlay</div>
-                <p>Product name</p>
-            </form>
-            <form class='products'>
-                <a href='#'><img src='#' alt='products'></a>
-                <div>price overlay</div>
-                <p>Product name</p>
-            </form>
-            <form class='products'>
-                <a href='#'><img src='#' alt='products'></a>
-                <div>price overlay</div>
-                <p>Product name</p>
-            </form>
-            <form class='products'>
-                <a href='#'><img src='#' alt='products'></a>
-                <div>price overlay</div>
-                <p>Product name</p>
-            </form>
-            <form class='products'>
-                <a href='#'><img src='#' alt='products'></a>
-                <div>price overlay</div>
-                <p>Product name</p>
-            </form>
-            <form class='products'>
-                <a href='#'><img src='#' alt='products'></a>
-                <div>price overlay</div>
-                <p>Product name</p>
-            </form>
-            <form class='products'>
-                <a href='#'><img src='#' alt='products'></a>
-                <div>price overlay</div>
-                <p>Product name</p>
-            </form>
+    <div class="row products_show">
+        <p><strong>You May Also Be Interested In:</strong></p>
+        <?php foreach ($similars as $similar) { ?>
+            <div class="col-sm-2 other_items">
+                <form class='products'>
+                    <a href='#'><img class="similar_item" src="/assets/images/<?= $similar['photo_url']  ?>" alt='products'></a>
+                    <p><?= $similar['name']  ?></p>
+                    <p><?= $similar['price']   ?></p>
+                </form>
+            </div>
+         <?php  } ?>
+    
+
     </div>
 </body>
 </html>
